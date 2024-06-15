f9c300d3bfc8b69eb6083c61ca8d7bc407f64e7e
Niraj Bhatta <niraj.bhatta@selu.edu>
2024-06-15 01:30:25 -0500

    Added Skills section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -27,7 +27,7 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4  text-white bg-black fixed  border-b border-teal-800 shadow-lg ">
+    <div className="flex justify-between items-center w-full h-20 px-4  text-white bg-black fixed top-0 left-0 z-50  border-b border-teal-800 shadow-lg ">
       <div>
         <h1 className="text-5xl font-signature ml-2"> Niraj </h1>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -49,10 +49,10 @@ const Projects = () => {
     };
 
     return (
-        <div name="projects" className="bg-gradient-to-b from-black to-gray-800 w-full text-white min-h-screen">
+        <div name="projects" className="bg-gradient-to-b from-black to-gray-800 w-full text-white min-h-screen pt-16">
             <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
                 <div className="pb-8">
-                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                    <p className="text-4xl font-bold inline border-b-4 border-teal-500">
                         Projects
                     </p>
                     <p className="py-6 font-bold">
